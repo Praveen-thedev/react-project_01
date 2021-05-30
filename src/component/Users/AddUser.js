f06c47e30cd6d61c3d3f86cc134b3a0fb9a4a16d
praveen-thedev <praveenteotia.teotia@gmail.com>
2021-05-30 17:35:56 +0530

    useRef instead of useState(unControlled Component)

--- src/component/Users/AddUser.js
+++ src/component/Users/AddUser.js
@@ -1,47 +1,24 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
-  const [userInput, setUserInput] = useState({
-    enteredFirstName: "",
-    enteredLastName: "",
-    enteredAge: "",
-    enteredCity: "",
-    enteredContact: "",
-  });
-  const addUserFNameHandler = (event) => {
-    setUserInput((prevState) => {
-      return { ...prevState, enteredFirstName: event.target.value };
-    });
-  };
-  const addUserLNameHandler = (event) => {
-    setUserInput((prevState) => {
-      return { ...prevState, enteredLastName: event.target.value };
-    });
-  };
-  const addUserAgeHandler = (event) => {
-    setUserInput((prevState) => {
-      return { ...prevState, enteredAge: event.target.value };
-    });
-  };
-  const addUserCityHandler = (event) => {
-    setUserInput((prevState) => {
-      return { ...prevState, enteredCity: event.target.value };
-    });
-  };
-  const addUserContactHandler = (event) => {
-    setUserInput((prevState) => {
-      return { ...prevState, enteredContact: event.target.value };
-    });
-  };
-
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const ageRef = useRef();
+  const cityRef = useRef();
+  const contactRef = useRef();
   const addUserHandler = (event) => {
     event.preventDefault();
+    const enteredUserFName = firstNameRef.current.value;
+    const enteredUserLName = lastNameRef.current.value;
+    const enteredUserAge = ageRef.current.value;
+    const enteredUserCity = cityRef.current.value;
+    const enteredUserContact = contactRef.current.value;
     if (
-      userInput.enteredFirstName.trim().length === 0 ||
-      userInput.enteredCity.trim().length === 0
+      enteredUserFName.trim().length === 0 ||
+      enteredUserCity.trim().length === 0
     ) {
       const errorBox = {
         title: "invalid input",
@@ -50,7 +27,7 @@ const AddUser = (props) => {
 
       return props.onAddError(errorBox);
     }
-    if (+userInput.enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       const errorBox = {
         title: "Invalid age",
         message: "please Enter a valid age( > 0)",
@@ -60,64 +37,34 @@ const AddUser = (props) => {
     }
     const userData = {
       id: Math.random().toString(),
-      fName: userInput.enteredFirstName,
-      lName: userInput.enteredLastName,
-      age: +userInput.enteredAge,
-      city: userInput.enteredCity,
-      contact: +userInput.enteredContact,
+      fName: enteredUserFName,
+      lName: enteredUserLName,
+      age: enteredUserAge,
+      city: enteredUserCity,
+      contact: enteredUserContact,
     };
     // console.log(userInput);
     props.onAddUser(userData);
-
-    setUserInput(() => {
-      return {
-        enteredFirstName: "",
-        enteredLastName: "",
-        enteredAge: "",
-        enteredCity: "",
-        enteredContact: "",
-      };
-    });
+    firstNameRef.current.value = "";
+    lastNameRef.current.value = "";
+    ageRef.current.value = "";
+    cityRef.current.value = "";
+    contactRef.current.value = "";
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="first_name">First Name</label>
-        <input
-          id="first_name"
-          type="text"
-          value={userInput.enteredFirstName}
-          onChange={addUserFNameHandler}
-        />
+        <input id="first_name" type="text" ref={firstNameRef} />
         <label htmlFor="last_name">Last Name</label>
-        <input
-          id="last_name"
-          type="text"
-          value={userInput.enteredLastName}
-          onChange={addUserLNameHandler}
-        />
+        <input id="last_name" type="text" ref={lastNameRef} />
         <label htmlFor="age">Age</label>
-        <input
-          id="age"
-          type="number"
-          value={userInput.enteredAge}
-          onChange={addUserAgeHandler}
-        />
+        <input id="age" type="number" ref={ageRef} />
         <label htmlFor="city_vill">City or Village</label>
-        <input
-          id="city_vill"
-          type="text"
-          value={userInput.enteredCity}
-          onChange={addUserCityHandler}
-        />
+        <input id="city_vill" type="text" ref={cityRef} />
         <label htmlFor="mobile_number">Mobile Number</label>
-        <input
-          id="mobile_number"
-          type="number"
-          value={userInput.enteredContact}
-          onChange={addUserContactHandler}
-        />
+        <input id="mobile_number" type="number" ref={contactRef} />
         <Button type="submit"> Add User </Button>
       </form>
     </Card>
